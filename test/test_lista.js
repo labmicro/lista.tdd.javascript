57cac51b3d70fde8ae9ad07b50f7ece7a6340a7b
Esteban Volentini <evolentini@equiser.com.ar>
2020-10-26 21:48:21 -0300

    Planificación de las pruebas que se implementarán para verificar el cumplimiento de los requisitos

---
+++ test/test_lista.js
@@ -0,0 +1,10 @@
+/**
+ * En una lista vacía hay cero elementos.
+ * Cuando se agrega un elemento a una lista vacía hay un elemento.
+ * En una lista vacía no se encuentra ninguna clave.
+ * Cuando se agrega un elemento a una lista vacía se puede recuperar el valor a partir de la clave.
+ * Cuando se agrega una clave que ya está en la lista se actualiza el valor correspondiente.
+ * Cuando se agregan un elemento a una lista vacía la lista de claves esta ordenada.
+ * Cuando se agrega un elemento al principio la lista de claves esta ordenada.
+ * Cuando se agrega un elemento al final la lista de claves esta ordenada.
+ */
